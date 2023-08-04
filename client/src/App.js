36cf30c56869ae16eb5aa150b5176c681210a398
Nicholas Suh <nicksuh17@gmail.com>
2023-08-04 16:03:03 -0700

    troll

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
 import React from "react";
 
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDP5rmqUlR-_j9qBAHcieSEwYoNbGFYZK4",
+  authDomain: "therapal-104ee.firebaseapp.com",
+  projectId: "therapal-104ee",
+  storageBucket: "therapal-104ee.appspot.com",
+  messagingSenderId: "482427393641",
+  appId: "1:482427393641:web:936f26917efec2306774f8",
+  measurementId: "G-4TTLV9F4D9"
+});
+
+const auth=firebase.auth();
+const firestore=firebase.firestore();
+
 export default function App() {
-    return <div>Hello World!!!</div>;
+    const [user] = useAuthState(auth);
+
+    return (
+        <div className="App">
+            Hello World!!!
+        </div>
+    )
+    
 }

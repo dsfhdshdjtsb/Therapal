34ec6b799494f1a700238f3b929406c6ca1df07c
Nicholas Suh <nicksuh17@gmail.com>
2023-08-04 16:47:56 -0700

    errors fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import 'firebase/auth';
-
+import { initializeApp } from "firebase/app"
+import { GoogleAuthProvider } from "firebase/auth"
 import { useAuthState } from 'react-firebase-hooks/auth';
-import {useCollectionData} from 'react-firebase-hooks/firestore';
+import { Routes, Route} from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import Test from "./components/test";
+import Test1 from "./components/test1";
 
-firebase.initializeApp({
+
+initializeApp({
   apiKey: "AIzaSyDP5rmqUlR-_j9qBAHcieSEwYoNbGFYZK4",
   authDomain: "therapal-104ee.firebaseapp.com",
   projectId: "therapal-104ee",
@@ -15,16 +21,42 @@ firebase.initializeApp({
   measurementId: "G-4TTLV9F4D9"
 });
 
-const auth=firebase.auth();
-const firestore=firebase.firestore();
+const auth=getAuth();
+const firestore=getFirestore();
 
 export default function App() {
     const [user] = useAuthState(auth);
 
     return (
-        <div className="App">
-            Hello World!!!
-        </div>
+        <Routes>
+            <Route exact path="/" element={<Test />} />
+            <Route path="/test" element={<Test1 />} />
+        </Routes>
+        
     )
     
+}
+
+function dummy(){
+    return (
+        <div className="App">
+            Hello World!
+        </div>
+    )
+}
+
+function SignIn(){
+    const signInWithGoogle=()=>{
+        const provider = new GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+    return (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+    )   
+}
+
+function SignOut(){
+    return auth.currentUser && (
+        <button onClick={()=>auth.signOut()}>Sign Out</button>
+    )
 }
---
+++ client/src/components/test.js
@@ -0,0 +1,7 @@
+export default function Test(){
+    return(
+        <div>
+            Hello World12312!!!nword
+        </div>
+    )
+}

d6a27836e7a6f91e8000b96363bb2b8612bbe3a3
Nicholas Suh <nicksuh17@gmail.com>
2023-08-04 15:45:48 -0700

    fireb ase setup

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDP5rmqUlR-_j9qBAHcieSEwYoNbGFYZK4",
+  authDomain: "therapal-104ee.firebaseapp.com",
+  projectId: "therapal-104ee",
+  storageBucket: "therapal-104ee.appspot.com",
+  messagingSenderId: "482427393641",
+  appId: "1:482427393641:web:936f26917efec2306774f8",
+  measurementId: "G-4TTLV9F4D9"
+});
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;

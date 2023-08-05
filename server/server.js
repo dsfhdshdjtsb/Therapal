a24f4493d4f8c988f4d25f4df7cf7ab85681eabf
Nicholas Suh <nicksuh17@gmail.com>
2023-08-05 15:55:11 -0700

    deleted node moduesl

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require('cors')
 var bodyParser = require('body-parser')
+const { Configuration, OpenAIApi } = require("openai");
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 var jsonParser = bodyParser.json()
+
+//exposed api key but idrc
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 app.use(cors({
     origin: "*"

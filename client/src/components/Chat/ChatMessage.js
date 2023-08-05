13773da98056f705efe7edca48c3537b837014c6
kYpranite <39254153+kYpranite@users.noreply.github.com>
2023-08-05 16:30:38 -0700

    finish chat ui

--- client/src/components/Chat/ChatMessage.js
+++ client/src/components/Chat/ChatMessage.js
@@ -2,31 +2,39 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-const messageStyles = {
-  sent: {
-    float: "right",
-    flexDirection: "row-reverse",
-  },
-  received: {
-    float: "left",
-  },
-};
-
-const textStyle = {
-  backgroundColor: "primary.main",
-  borderRadius: "10px",
-  padding: "7px",
-  margin: "0 10px 30px 10px",
-  width: "90%"
-};
-
 export default function ChatMessage(props) {
-  return(
-      <Box display="flex" sx={messageStyles.sent}>
-        <AccountCircleIcon fontSize="large" />
+  console.log(props.sent);
+  let messageStyles = {};
+  let textStyle = {
+    borderRadius: "10px",
+    padding: "7px",
+    margin: "0 10px 25px 10px",
+    maxWidth: "90%",
+  };
+  if (props.sent) {
+    messageStyles = {
+      float: "right",
+      flexDirection: "row-reverse",
+    };
+    textStyle = { ...textStyle, backgroundColor: "primary.main" };
+  } else {
+    messageStyles = {
+      float: "left",
+    };
+    textStyle = { ...textStyle, backgroundColor: "#D3D3D3" };
+  }
+
+  return (
+    <React.Fragment>
+      <Box display="flex" sx={messageStyles} padding="0 3% 0 3%">
+        <Typography variant="body1" sx={{fontWeight: "light", fontSize: "0.8rem"}}>{props.name}</Typography>
+      </Box>
+      <Box display="flex" sx={messageStyles}>
+        <AccountCircleIcon fontSize="large" color="accent" />
         <Typography variant="body1" sx={textStyle}>
           {props.text}
         </Typography>
       </Box>
+    </React.Fragment>
   );
 }
--- client/src/components/Chat/ChatWindow.js
+++ client/src/components/Chat/ChatWindow.js
@@ -5,11 +5,17 @@ import ChatMessage from "./ChatMessage";
 
 function ChatWindow() {
   return (
-    <Box sx={{ position: "relative", flexGrow: 1, overflow: "auto"}}>
+    <Box sx={{ position: "relative", flexGrow: 1, overflow: "auto" }}>
       <Toolbar />
       <PromptBar />
-      <Box p={"2%"}>
-        <ChatMessage />
+      <Box p={"2%"} display={"flex"} flexDirection={"column"}>
+        <ChatMessage sent={true} text="TEST" />
+        <ChatMessage
+          sent={false}
+          text="Maecenas at felis non turpis scelerisque gravida."
+        />
+        <ChatMessage sent={false} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at felis non turpis scelerisque gravida" />
+        <ChatMessage sent={true} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at felis non turpis scelerisque gravida. Suspendisse potenti. Proin sodales arcu id nunc tempus porttitor. Sed scelerisque justo nec tempus pulvinar. Cras faucibus vulputate sapien, sit amet scelerisque justo mollis vitae. Sed id fringilla felis. Cras venenatis mi non mauris consequat, pharetra elementum dui rhoncus. Integer dictum urna eu ultricies tincidunt. Maecenas placerat magna ut tortor auctor, nec tempor nibh finibus." />
       </Box>
     </Box>
   );

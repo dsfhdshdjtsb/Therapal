4fc197a6467adc5ad764340c632eacdd03d6148b
kYpranite <39254153+kYpranite@users.noreply.github.com>
2023-08-06 04:15:03 -0700

    adjust prompt and add default prompt

--- client/src/components/Chat/ChatWindow.js
+++ client/src/components/Chat/ChatWindow.js
@@ -6,16 +6,14 @@ import ChatMessage from "./ChatMessage";
 
 function ChatWindow(props) {
   const scrollDiv = useRef();
-  const [prompt, setPrompt] = React.useState(null);
+  const [prompt, setPrompt] = React.useState("TheraPal hasn't been prompted yet! If you want to enhance your conversation, press the Generate Prompt button!");
   useEffect(()=>{
     if (props.messages && props.messages.length > 0){
-      console.log(props.messages[props.messages.length-1].t)
+      console.log(props.messages[props.messages.length-1].text)
       if (props.messages[props.messages.length-1].uid === "ChatGPT"){
         setPrompt(props.messages[props.messages.length-1].text);
       }
-    }else if(prompt === null){
-      
-    }
+    } 
   }, [props.messages])
 
   function renderMessage(msg) {
